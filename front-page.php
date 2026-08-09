613d1ee290915c1d8c3c20a8fbd1fd8a0603ef53
Aleksandr Khimich <djmurlok.sf@gmail.com>
2026-08-09 16:47:32 +0000

    fix_front_page

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+get_header();
+?>
+
+<section class="home">
+    <div class="container">
+        <h1>Имидж Строй</h1>
+        <p>Главная страница кастомной темы работает.</p>
+    </div>
+</section>
+
+<?php
+get_footer();
